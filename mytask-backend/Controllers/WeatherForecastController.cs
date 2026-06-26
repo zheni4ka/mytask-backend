99361044dbe79b927bea510f324b94588febc378
zheni4ka <ebranik15@gmail.com>
2026-06-26 22:29:44 +0300

    Add project files.

---
+++ mytask-backend/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mytask_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : Controller
+    {
+
+    }
+}
